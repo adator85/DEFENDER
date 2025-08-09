f3fe3c43cbf5af1842b468ed589fd48132720002
adator <85586985+adator85@users.noreply.github.com>
2025-08-09 03:48:56 +0200

    updating namings in admin.py to stay coherent

--- core/classes/admin.py
+++ core/classes/admin.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional, Union
 from core.base import Base
 import core.definition as df
 
@@ -6,121 +6,109 @@ class Admin:
 
     UID_ADMIN_DB: list[df.MAdmin] = []
 
-    def __init__(self, baseObj: Base) -> None:
-        self.Logs = baseObj.logs
-        pass
+    def __init__(self, base: Base) -> None:
+        self.Logs = base.logs
 
-    def insert(self, newAdmin: df.MAdmin) -> bool:
+    def insert(self, new_admin: df.MAdmin) -> bool:
 
         result = False
         exist = False
 
         for record in self.UID_ADMIN_DB:
-            if record.uid == newAdmin.uid:
+            if record.uid == new_admin.uid:
                 # If the admin exist then return False and do not go further
                 exist = True
                 self.Logs.debug(f'{record.uid} already exist')
                 return result
 
         if not exist:
-            self.UID_ADMIN_DB.append(newAdmin)
+            self.UID_ADMIN_DB.append(new_admin)
             result = True
-            self.Logs.debug(f'UID ({newAdmin.uid}) has been created')
+            self.Logs.debug(f'UID ({new_admin.uid}) has been created')
 
         if not result:
-            self.Logs.critical(f'The User Object was not inserted {newAdmin}')
+            self.Logs.critical(f'The User Object was not inserted {new_admin}')
 
         return result
 
-    def update_nickname(self, uid: str, newNickname: str) -> bool:
+    def update_nickname(self, uid: str, new_admin_nickname: str) -> bool:
 
         result = False
 
         for record in self.UID_ADMIN_DB:
             if record.uid == uid:
                 # If the admin exist, update and do not go further
-                record.nickname = newNickname
+                record.nickname = new_admin_nickname
                 result = True
-                self.Logs.debug(f'UID ({record.uid}) has been updated with new nickname {newNickname}')
+                self.Logs.debug(f'UID ({record.uid}) has been updated with new nickname {new_admin_nickname}')
                 return result
 
         if not result:
-            self.Logs.debug(f'The new nickname {newNickname} was not updated, uid = {uid} - The Client is not an admin')
+            self.Logs.debug(f'The new nickname {new_admin_nickname} was not updated, uid = {uid} - The Client is not an admin')
 
         return result
 
-    def update_level(self, nickname: str, newLevel: int) -> bool:
+    def update_level(self, nickname: str, new_admin_level: int) -> bool:
 
         result = False
 
         for record in self.UID_ADMIN_DB:
             if record.nickname == nickname:
                 # If the admin exist, update and do not go further
-                record.level = newLevel
+                record.level = new_admin_level
                 result = True
-                self.Logs.debug(f'Admin ({record.nickname}) has been updated with new level {newLevel}')
+                self.Logs.debug(f'Admin ({record.nickname}) has been updated with new level {new_admin_level}')
                 return result
 
         if not result:
-            self.Logs.debug(f'The new level {newLevel} was not updated, nickname = {nickname} - The Client is not an admin')
+            self.Logs.debug(f'The new level {new_admin_level} was not updated, nickname = {nickname} - The Client is not an admin')
 
         return result
 
     def delete(self, uidornickname: str) -> bool:
 
-        result = False
-
         for record in self.UID_ADMIN_DB:
             if record.uid == uidornickname:
                 # If the admin exist, delete and do not go further
                 self.UID_ADMIN_DB.remove(record)
-                result = True
                 self.Logs.debug(f'UID ({record.uid}) has been deleted')
-                return result
+                return True
             if record.nickname == uidornickname:
                 # If the admin exist, delete and do not go further
                 self.UID_ADMIN_DB.remove(record)
-                result = True
                 self.Logs.debug(f'nickname ({record.nickname}) has been deleted')
-                return result
+                return True
 
-        if not result:
-            self.Logs.critical(f'The UID {uidornickname} was not deleted')
+        self.Logs.critical(f'The UID {uidornickname} was not deleted')
 
-        return result
+        return False
 
-    def get_Admin(self, uidornickname: str) -> Union[df.MAdmin, None]:
+    def get_Admin(self, uidornickname: str) -> Optional[df.MAdmin]:
 
-        Admin = None
         for record in self.UID_ADMIN_DB:
             if record.uid == uidornickname:
-                Admin = record
+                return record
             elif record.nickname == uidornickname:
-                Admin = record
+                return record
 
-        #self.Logs.debug(f'Search {uidornickname} -- result = {Admin}')
+        return None
 
-        return Admin
+    def get_uid(self, uidornickname:str) -> Optional[str]:
 
-    def get_uid(self, uidornickname:str) -> Union[str, None]:
-
-        uid = None
         for record in self.UID_ADMIN_DB:
             if record.uid == uidornickname:
-                uid = record.uid
+                return record.uid
             if record.nickname == uidornickname:
-                uid = record.uid
+                return record.uid
 
-        self.Logs.debug(f'The UID that you are looking for {uidornickname} has been found {uid}')
-        return uid
+        return None
 
-    def get_nickname(self, uidornickname:str) -> Union[str, None]:
+    def get_nickname(self, uidornickname:str) -> Optional[str]:
 
-        nickname = None
         for record in self.UID_ADMIN_DB:
             if record.nickname == uidornickname:
-                nickname = record.nickname
+                return record.nickname
             if record.uid == uidornickname:
-                nickname = record.nickname
-        self.Logs.debug(f'The value {uidornickname} -- {nickname}')
-        return nickname
+                return record.nickname
+
+        return None
