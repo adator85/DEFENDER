808511891932442f5ba7cb305c2db177423af1db
adator <85586985+adator85@users.noreply.github.com>
2026-02-09 01:39:17 +0100

    Passing flake8 test on the test module.

--- mods/test/mod_test.py
+++ mods/test/mod_test.py
@@ -1,10 +1,11 @@
 import asyncio
-from typing import Any, TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional
 from core.classes.interfaces.imodule import IModule
 from dataclasses import dataclass
 
 if TYPE_CHECKING:
     from core.loader import Loader
+
 
 class Test(IModule):
 
@@ -12,17 +13,18 @@ class Test(IModule):
     class ModConfModel:
         """The Model containing the module parameters (Mandatory)
         you can leave it without params.
-        just use pass | if you leave it empty, in the load() method just init empty object ==> self.ModConfig = ModConfModel()
+        just use pass | if you leave it empty, in the load() method just
+        init empty object ==> self.ModConfig = ModConfModel()
         """
         param_exemple1: str
         param_exemple2: int
 
     MOD_HEADER: dict[str, str] = {
-        'name':'Test',
-        'version':'1.0.0',
-        'description':'The test module',
-        'author':'Defender Team',
-        'core_version':'Defender-6'
+        'name': 'Test',
+        'version': '1.0.0',
+        'description': 'The test module',
+        'author': 'Defender Team',
+        'core_version': 'Defender-6'
     }
     """Module Header (Mandatory)"""
 
@@ -36,18 +38,19 @@ class Test(IModule):
 
     async def create_tables(self) -> None:
         """Methode qui va créer la base de donnée si elle n'existe pas.
-           Une Session unique pour cette classe sera crée, qui sera utilisé dans cette classe / module
+           Une Session unique pour cette classe sera crée, qui sera utilisé
+           dans cette classe / module
 
         Returns:
             None: Aucun retour n'es attendu
         """
 
-        table_logs = '''CREATE TABLE IF NOT EXISTS test_logs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            datetime TEXT,
-            server_msg TEXT
-            )
-        '''
+        # table_logs = '''CREATE TABLE IF NOT EXISTS test_logs (
+        #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+        #     datetime TEXT,
+        #     server_msg TEXT
+        #     )
+        # '''
 
         # await self.ctx.Base.db_execute_query(table_logs)
         return None
@@ -59,33 +62,46 @@ class Test(IModule):
         await self.create_tables()
 
         # Create module commands (Mandatory)
-        self.ctx.Commands.build_command(0, self.module_name, 'test-command', 'Execute a test command')
-        self.ctx.Commands.build_command(0, self.module_name, 'asyncio', 'Create a new asynchron task!')
-        self.ctx.Commands.build_command(1, self.module_name, 'test_level_1', 'Execute a level 1 test command')
-        self.ctx.Commands.build_command(2, self.module_name, 'test_level_2', 'Execute a level 2 test command')
-        self.ctx.Commands.build_command(3, self.module_name, 'test_level_3', 'Execute a level 3 test command')
+        _commands = self.ctx.Commands
+        _commands.build_command(0, self.module_name, 'test-command',
+                                'Execute a test command')
+        _commands.build_command(0, self.module_name, 'asyncio',
+                                'Create a new asynchron task!')
+        _commands.build_command(1, self.module_name, 'test_level_1',
+                                'Execute a level 1 test command')
+        _commands.build_command(2, self.module_name, 'test_level_2',
+                                'Execute a level 2 test command')
+        _commands.build_command(3, self.module_name, 'test_level_3',
+                                'Execute a level 3 test command')
 
         # Build the default configuration model (Mandatory)
-        self._mod_config = self.ModConfModel(param_exemple1='str', param_exemple2=1)
+        self._mod_config = self.ModConfModel(param_exemple1='str',
+                                             param_exemple2=1)
 
         # sync the database with local variable (Mandatory)
         await self.sync_db()
 
         if self.mod_config.param_exemple2 == 1:
-            await self.ctx.Irc.Protocol.send_priv_msg(self.ctx.Config.SERVICE_NICKNAME, "Param activated", self.ctx.Config.SERVICE_CHANLOG)
+            await self.ctx.Irc.Protocol.send_priv_msg(
+                self.ctx.Config.SERVICE_NICKNAME,
+                "Param activated",
+                self.ctx.Config.SERVICE_CHANLOG)
 
     def unload(self) -> None:
-        """### This method is called when you unload, or you reload the module (Mandatory)"""
+        """### This method is called when you unload,
+        or you reload the module (Mandatory)"""
         self.ctx.Commands.drop_command_by_module(self.module_name)
         return None
 
     async def asyncio_func(self) -> None:
-        self.ctx.Logs.debug(f"Starting async method in a task: {self.__class__.__name__}")
+        self.ctx.Logs.debug(
+            f"Starting async method in a task: {self.__class__.__name__}")
         await asyncio.sleep(2)
         self.ctx.Logs.debug(f"End of the task: {self.__class__.__name__}")
 
     def cmd(self, data: list[str]) -> None:
-        """All messages coming from the IRCD server will be handled using this method (Mandatory)
+        """All messages coming from the IRCD server will be handled using
+        this method (Mandatory)
 
         Args:
             data (list): Messages coming from the IRCD server.
@@ -101,17 +117,21 @@ class Test(IModule):
         except Exception as err:
             self.ctx.Logs.error(f"General Error: {err}")
 
-    async def hcmds(self, user: str, channel: Optional[str], cmd: list, fullcmd: Optional[list]) -> None:
+    async def hcmds(self, user: str, channel: Optional[str],
+                    cmd: list, fullcmd: Optional[list]) -> None:
         """All messages coming from the user commands (Mandatory)
 
         Args:
             user (str): The user who send the request.
-            channel (Any): The channel from where is coming the message (could be None).
+            channel (Any): The channel from where is coming the message
+                (could be None).
             cmd (list): The messages coming from the IRCD server.
-            fullcmd (list, optional): The full messages coming from the IRCD server. Defaults to [].
+            fullcmd (list, optional): The full messages coming from the
+                IRCD server. Defaults to [].
         """
         u = self.ctx.User.get_user(user)
-        c = self.ctx.Channel.get_channel(channel) if self.ctx.Channel.is_valid_channel(channel) else None
+        c = (self.ctx.Channel.get_channel(channel)
+             if self.ctx.Channel.is_valid_channel(channel) else None)
         if u is None:
             return None
 
@@ -119,25 +139,35 @@ class Test(IModule):
         dnickname = self.ctx.Config.SERVICE_NICKNAME
 
         match command:
-            
+
             case 'asyncio':
                 self.ctx.DAsyncio.create_safe_task(self.asyncio_func())
                 return None
 
             case 'test-command':
                 try:
-                    await self.ctx.Irc.Protocol.send_notice(nick_from=dnickname, nick_to=u.nickname, msg="This is a notice to the sender ...")
-                    await self.ctx.Irc.Protocol.send_priv_msg(nick_from=dnickname, msg=f"This is private message to the sender ...", nick_to=u.nickname)
+                    await self.ctx.Irc.Protocol.send_notice(
+                        nick_from=dnickname,
+                        nick_to=u.nickname,
+                        msg="This is a notice to the sender ...")
+                    await self.ctx.Irc.Protocol.send_priv_msg(
+                        nick_from=dnickname,
+                        msg="This is private message to the sender ...",
+                        nick_to=u.nickname)
 
                     if c is not None:
-                        await self.ctx.Irc.Protocol.send_priv_msg(nick_from=dnickname, msg=f"This is private message to the sender ...", channel=c.name)
+                        await self.ctx.Irc.Protocol.send_priv_msg(
+                            nick_from=dnickname,
+                            msg="This is private message to the sender ...",
+                            channel=c.name)
 
                     # How to update your module configuration
                     await self.update_configuration('param_exemple2', 7)
-                    await self.update_configuration('param_exemple1', 'my_value')
+                    await self.update_configuration('param_exemple1',
+                                                    'my_value')
 
                     # Log if you want the result
-                    self.ctx.Logs.debug(f"Test logs ready")
+                    self.ctx.Logs.debug("Test logs ready")
                     return None
 
                 except Exception as err:
